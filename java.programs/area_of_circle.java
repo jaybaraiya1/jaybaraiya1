25f1dcda834821f61f433e1fa78a863b36cd908c
jay baraiya <140989637+jaybaraiya1@users.noreply.github.com>
2024-12-23 21:13:50 +0530

    Add files via upload

---
+++ java.programs/area_of_circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class area_of_circle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("r of circle");
+        int r=scanner.nextInt();
+        double area=3.14*r*r;
+        System.out.println("Area of circle is: "+area);
+    }
+}
---
+++ java.programs/sum.java
@@ -0,0 +1,11 @@
+public class sum {
+
+    public static void main(String[] args) {
+        
+        int a,b;
+        a=9;
+        b=8;
+        System.out.println(a+b);
+    }
+    }
+  
